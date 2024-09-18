2e1ea7b3ed306c4c2e172f7a5b950fa83e11014f
Chen Kai <281165273grape@gmail.com>
2024-09-18 22:16:09 +0800

    feat:Add minimal preset
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const preset=@import("presets/preset.zig");
+const preset = @import("presets/preset.zig");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
