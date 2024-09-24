49159e509a9cb69e4be1e8086a1e685788ae97a9
Chen Kai <281165273grape@gmail.com>
2024-09-24 18:15:37 +0800

    feat:init hardfork type
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- src/root.zig
+++ src/root.zig
@@ -4,6 +4,12 @@ pub const types = @import("primitives/types.zig");
 pub const constants = @import("primitives/constants.zig");
 pub const utils = @import("primitives/utils.zig");
 pub const consensus = @import("consensus/types.zig");
+pub const phase0 = @import("consensus/phase0/types.zig");
+pub const altair = @import("consensus/altair/types.zig");
+pub const bellatrix = @import("consensus/bellatrix/types.zig");
+pub const capella = @import("consensus/capella/types.zig");
+pub const deneb = @import("consensus/deneb/types.zig");
+pub const electra = @import("consensus/electra/types.zig");
 
 test {
     @import("std").testing.refAllDeclsRecursive(@This());
