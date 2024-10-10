e1b7d10fd0ca98bc20f5b0b90892ee531d7bdf7c
Chen Kai <281165273grape@gmail.com>
2024-10-10 22:54:57 +0800

    feat:add more seed helper
    Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- src/consensus/helpers/seed.zig
+++ src/consensus/helpers/seed.zig
@@ -7,6 +7,7 @@ const preset = @import("../../presets/preset.zig");
 const phase0 = @import("../../consensus/phase0/types.zig");
 const altair = @import("../../consensus/altair/types.zig");
 const epoch_helper = @import("../../consensus/helpers/epoch.zig");
+const sha256 = std.crypto.hash.sha2.Sha256;
 
 /// getRandaoMix returns the randao mix at the given epoch.
 /// @param state - The state.
@@ -41,7 +42,7 @@ pub fn getSeed(state: *const consensus.BeaconState, epoch: primitives.Epoch, dom
     std.mem.writeInt(primitives.Epoch, input[primitives.DomainTypeSize .. primitives.DomainTypeSize + primitives.EpochSize], epoch, .little);
     @memcpy(input[primitives.DomainTypeSize + primitives.EpochSize ..], &mix);
     var h: primitives.Bytes32 = undefined;
-    std.crypto.hash.sha2.Sha256.hash(&input, &h, .{});
+    sha256.hash(&input, &h, .{});
     return h;
 }
 
@@ -82,7 +83,7 @@ pub fn computeShuffledIndex(index: u64, index_count: u64, seed: primitives.Bytes
     for (@as(u64, 0)..preset.ActivePreset.get().SHUFFLE_ROUND_COUNT) |current_round| {
         // Generate round seed
         var round_seed: primitives.Bytes32 = undefined;
-        std.crypto.hash.sha2.Sha256.hash(seed ++ &[_]u8{@as(u8, @intCast(current_round))}, &round_seed, .{});
+        sha256.hash(seed ++ &[_]u8{@as(u8, @intCast(current_round))}, &round_seed, .{});
 
         // Calculate pivot and flip
         const pivot = @mod(std.mem.readInt(u64, round_seed[0..8], .little), index_count);
@@ -92,7 +93,7 @@ pub fn computeShuffledIndex(index: u64, index_count: u64, seed: primitives.Bytes
         // Generate source seed
         var source_seed: primitives.Bytes32 = undefined;
         const position_div_256 = @as(u32, @intCast(@divFloor(position, 256)));
-        std.crypto.hash.sha2.Sha256.hash(seed ++ &[_]u8{@as(u8, @intCast(current_round))} ++ std.mem.toBytes(position_div_256), &source_seed, .{});
+        sha256.hash(seed ++ &[_]u8{@as(u8, @intCast(current_round))} ++ std.mem.toBytes(position_div_256), &source_seed, .{});
 
         // Determine bit value and update current_index
         const byte_index = @divFloor(@mod(position, 256), 8);
@@ -104,6 +105,35 @@ pub fn computeShuffledIndex(index: u64, index_count: u64, seed: primitives.Bytes
     }
 
     return current_index;
+}
+
+pub fn computeProposerIndex(state: *const consensus.BeaconState, indices: []const primitives.ValidatorIndex, seed: primitives.Bytes32) !primitives.ValidatorIndex {
+    if (indices.len == 0) return error.EmptyValidatorIndices;
+    const MAX_RANDOM_BYTE: u8 = std.math.maxInt(u8);
+    var i: u64 = 0;
+    const total: u64 = indices.len;
+
+    while (true) {
+        const shuffled_index = try computeShuffledIndex(@mod(i, total), total, seed);
+        const candidate_index = indices[@intCast(shuffled_index)];
+        var hash_result: [32]u8 = undefined;
+        var seed_plus: [40]u8 = undefined;
+        @memcpy(seed_plus[0..32], &seed);
+        std.mem.writeInt(u64, seed_plus[32..40], @divFloor(i, 32), .little);
+        std.debug.print("seed_plus: {any}, i: {}\n", .{ seed_plus, i });
+        std.crypto.hash.sha2.Sha256.hash(&seed_plus, &hash_result, .{});
+        const randomByte = hash_result[@mod(i, 32)];
+        const effectiveBalance = state.validators()[candidate_index].effective_balance;
+
+        const max_effective_balance = switch (state.*) {
+            .electra => preset.ActivePreset.get().MAX_EFFECTIVE_BALANCE_ELECTRA,
+            else => preset.ActivePreset.get().MAX_EFFECTIVE_BALANCE,
+        };
+        if (effectiveBalance * MAX_RANDOM_BYTE >= max_effective_balance * randomByte) {
+            return candidate_index;
+        }
+        i += 1;
+    }
 }
 
 test "test get_randao_mix" {
@@ -260,4 +290,76 @@ test "test computeShuffledIndex" {
     const index1 = 5776655;
     const shuffledIndex1 = try computeShuffledIndex(index1, index_count1, seed1);
     try std.testing.expectEqual(3446028, shuffledIndex1);
+}
+
+test "test computeProposerIndex" {
+    preset.ActivePreset.set(preset.Presets.minimal);
+    defer preset.ActivePreset.reset();
+    var finalized_checkpoint = consensus.Checkpoint{
+        .epoch = 5,
+        .root = .{0} ** 32,
+    };
+    var validators = std.ArrayList(consensus.Validator).init(std.testing.allocator);
+    defer validators.deinit();
+    const validator1 = consensus.Validator{
+        .pubkey = undefined,
+        .withdrawal_credentials = undefined,
+        .effective_balance = 12312312312,
+        .slashed = false,
+        .activation_eligibility_epoch = 0,
+        .activation_epoch = 0,
+        .exit_epoch = 0,
+        .withdrawable_epoch = 0,
+    };
+    try validators.append(validator1);
+    const validator2 = consensus.Validator{
+        .pubkey = undefined,
+        .withdrawal_credentials = undefined,
+        .effective_balance = 232323232332,
+        .slashed = false,
+        .activation_eligibility_epoch = 0,
+        .activation_epoch = 0,
+        .exit_epoch = 0,
+        .withdrawable_epoch = 0,
+    };
+    try validators.append(validator2);
+
+    var randao_mixes = try std.ArrayList(primitives.Bytes32).initCapacity(std.testing.allocator, preset.ActivePreset.get().EPOCHS_PER_HISTORICAL_VECTOR);
+    defer randao_mixes.deinit();
+    for (0..preset.ActivePreset.get().EPOCHS_PER_HISTORICAL_VECTOR) |slot_index| {
+        try randao_mixes.append(.{@as(u8, @intCast(slot_index))} ** 32);
+    }
+
+    const state = consensus.BeaconState{
+        .altair = altair.BeaconState{
+            .genesis_time = 0,
+            .genesis_validators_root = .{0} ** 32,
+            .slot = 100,
+            .fork = undefined,
+            .block_roots = undefined,
+            .state_roots = undefined,
+            .historical_roots = undefined,
+            .eth1_data = undefined,
+            .eth1_data_votes = undefined,
+            .eth1_deposit_index = 0,
+            .validators = validators.items,
+            .balances = undefined,
+            .randao_mixes = randao_mixes.items,
+            .slashings = undefined,
+            .previous_epoch_attestations = undefined,
+            .current_epoch_attestations = undefined,
+            .justification_bits = undefined,
+            .previous_justified_checkpoint = undefined,
+            .current_justified_checkpoint = undefined,
+            .finalized_checkpoint = &finalized_checkpoint,
+            .latest_block_header = undefined,
+            .inactivity_scores = undefined,
+            .current_sync_committee = undefined,
+            .next_sync_committee = undefined,
+        },
+    };
+
+    const validator_index = [_]primitives.ValidatorIndex{ 0, 1 };
+    const proposer_index = try computeProposerIndex(&state, &validator_index, .{1} ** 32);
+    try std.testing.expectEqual(0, proposer_index);
 }
