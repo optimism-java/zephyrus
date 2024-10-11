6f01670602c5261fa654e5a3b20bf34b328c493f
fearlessfe <505380967@qq.com>
2024-10-11 23:48:48 +0800

    fix: hashTreeRoot error

--- build.zig
+++ build.zig
@@ -95,10 +95,19 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
+    const test_cases = b.addTest(.{
+        .root_source_file = b.path("src/tests.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_test_cases = b.addRunArtifact(test_cases);
+
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_test_cases.step);
 }
