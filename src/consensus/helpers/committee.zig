49eb52d9b8664c606044747e810cdcb2e5e2a312
Chen Kai <281165273grape@gmail.com>
2024-10-27 12:47:07 +0800

    feat:add getNextSyncCommitteeIndices Signed-off-by: Chen Kai <281165273grape@gmail.com>

--- src/consensus/helpers/committee.zig
+++ src/consensus/helpers/committee.zig
@@ -127,6 +127,70 @@ pub fn getBeaconProposerIndex(state: *const consensus.BeaconState, allocator: st
     return validator_helper.computeProposerIndex(state, indices, seed);
 }
 
+/// getNextSyncCommitteeIndices returns the next sync committee indices.
+/// @param state - The beacon state.
+/// @param allocator - The allocator.
+/// @returns The next sync committee indices.
+/// Spec pseudocode definition:
+/// def get_next_sync_committee_indices(state: BeaconState) -> Sequence[ValidatorIndex]:
+///     """
+///     Return the sync committee indices, with possible duplicates, for the next sync committee.
+///     """
+///     epoch = Epoch(get_current_epoch(state) + 1)
+///
+///     MAX_RANDOM_BYTE = 2**8 - 1
+///     active_validator_indices = get_active_validator_indices(state, epoch)
+///     active_validator_count = uint64(len(active_validator_indices))
+///     seed = get_seed(state, epoch, DOMAIN_SYNC_COMMITTEE)
+///     i = 0
+///     sync_committee_indices: List[ValidatorIndex] = []
+///     while len(sync_committee_indices) < SYNC_COMMITTEE_SIZE:
+///        shuffled_index = compute_shuffled_index(uint64(i % active_validator_count), active_validator_count, seed)
+///        candidate_index = active_validator_indices[shuffled_index]
+///        random_byte = hash(seed + uint_to_bytes(uint64(i // 32)))[i % 32]
+///        effective_balance = state.validators[candidate_index].effective_balance
+///        # [Modified in Electra:EIP7251]
+///        if effective_balance * MAX_RANDOM_BYTE >= MAX_EFFECTIVE_BALANCE_ELECTRA * random_byte:
+///            sync_committee_indices.append(candidate_index)
+///        i += 1
+///     return sync_committee_indices
+pub fn getNextSyncCommitteeIndices(state: *consensus.BeaconState, allocator: std.mem.Allocator) ![]primitives.ValidatorIndex {
+    const epoch = @as(primitives.Epoch, epoch_helper.getCurrentEpoch(state) + 1);
+    const MAX_RANDOM_BYTE = std.math.maxInt(u8);
+
+    const active_validator_indices = try validator_helper.getActiveValidatorIndices(state, epoch, allocator);
+    defer allocator.free(active_validator_indices);
+    const active_validator_count = @as(u64, active_validator_indices.len);
+    const seed = seed_helper.getSeed(state, epoch, constants.DOMAIN_SYNC_COMMITTEE);
+
+    // Fixed-size buffer allocation
+    var result = try allocator.alloc(primitives.ValidatorIndex, preset.ActivePreset.get().SYNC_COMMITTEE_SIZE);
+    var count: usize = 0;
+    var i: usize = 0;
+
+    while (count < preset.ActivePreset.get().SYNC_COMMITTEE_SIZE) {
+        const shuffled_index = try shuffle_helper.computeShuffledIndex(@as(u64, @mod(i, active_validator_count)), active_validator_count, seed);
+        const candidate_index = active_validator_indices[shuffled_index];
+        const seed_origin = seed ++ std.mem.asBytes(&@as(u64, @divFloor(i, 32)));
+        var seed_hash: primitives.Bytes32 = undefined;
+        sha256.hash(seed_origin, &seed_hash, .{});
+        const random_byte = seed_hash[@mod(i, 32)];
+        const effective_balance = state.validators()[candidate_index].effective_balance;
+
+        const max_effective_balance = if (state.* == .electra)
+            preset.ActivePreset.get().MAX_EFFECTIVE_BALANCE_ELECTRA
+        else
+            preset.ActivePreset.get().MAX_EFFECTIVE_BALANCE;
+
+        if (effective_balance * MAX_RANDOM_BYTE >= max_effective_balance * random_byte) {
+            result[count] = candidate_index;
+            count += 1;
+        }
+        i += 1;
+    }
+    return result;
+}
+
 test "test getCommitteeCountPerSlot" {
     preset.ActivePreset.set(preset.Presets.minimal);
     defer preset.ActivePreset.reset();
@@ -381,4 +445,90 @@ test "test getBeaconProposerIndex" {
 
     const proposer_index = try getBeaconProposerIndex(&state, std.testing.allocator);
     try std.testing.expectEqual(674517, proposer_index);
+}
+
+test "test getNextSyncCommitteeIndices" {
+    preset.ActivePreset.set(preset.Presets.minimal);
+    defer preset.ActivePreset.reset();
+    const finalized_checkpoint = consensus.Checkpoint{
+        .epoch = 5,
+        .root = .{0} ** 32,
+    };
+    var validators = std.ArrayList(consensus.Validator).init(std.testing.allocator);
+    defer validators.deinit();
+    const validator1 = consensus.Validator{
+        .pubkey = undefined,
+        .withdrawal_credentials = undefined,
+        .effective_balance = 0,
+        .slashed = false,
+        .activation_eligibility_epoch = 0,
+        .activation_epoch = 0,
+        .exit_epoch = 10,
+        .withdrawable_epoch = 10,
+    };
+    const validator2 = consensus.Validator{
+        .pubkey = undefined,
+        .withdrawal_credentials = undefined,
+        .effective_balance = 0,
+        .slashed = false,
+        .activation_eligibility_epoch = 0,
+        .activation_epoch = 0,
+        .exit_epoch = 20,
+        .withdrawable_epoch = 20,
+    };
+    for (0..500000) |_| {
+        try validators.append(validator1);
+        try validators.append(validator2);
+    }
+    var block_roots = std.ArrayList(primitives.Root).init(std.testing.allocator);
+    defer block_roots.deinit();
+    const block_root1 = .{0} ** 32;
+    const block_root2 = .{1} ** 32;
+    const block_root3 = .{2} ** 32;
+    try block_roots.append(block_root1);
+    try block_roots.append(block_root2);
+    try block_roots.append(block_root3);
+    var randao_mixes = try std.ArrayList(primitives.Bytes32).initCapacity(std.testing.allocator, preset.ActivePreset.get().EPOCHS_PER_HISTORICAL_VECTOR);
+    defer randao_mixes.deinit();
+    for (0..preset.ActivePreset.get().EPOCHS_PER_HISTORICAL_VECTOR) |slot_index| {
+        try randao_mixes.append(.{@as(u8, @intCast(slot_index))} ** 32);
+    }
+
+    var state = consensus.BeaconState{
+        .altair = altair.BeaconState{
+            .genesis_time = 0,
+            .genesis_validators_root = .{0} ** 32,
+            .slot = 100,
+            .fork = undefined,
+            .block_roots = block_roots.items,
+            .state_roots = undefined,
+            .historical_roots = undefined,
+            .eth1_data = undefined,
+            .eth1_data_votes = undefined,
+            .eth1_deposit_index = 0,
+            .validators = validators.items,
+            .balances = undefined,
+            .randao_mixes = randao_mixes.items,
+            .slashings = undefined,
+            .previous_epoch_attestations = undefined,
+            .current_epoch_attestations = undefined,
+            .justification_bits = undefined,
+            .previous_justified_checkpoint = undefined,
+            .current_justified_checkpoint = undefined,
+            .finalized_checkpoint = finalized_checkpoint,
+            .latest_block_header = undefined,
+            .inactivity_scores = undefined,
+            .current_sync_committee = undefined,
+            .next_sync_committee = undefined,
+        },
+    };
+
+    const indices = try getNextSyncCommitteeIndices(&state, std.testing.allocator);
+    defer std.testing.allocator.free(indices);
+    try std.testing.expectEqual(32, indices.len);
+    try std.testing.expectEqual(864147, indices[0]);
+    try std.testing.expectEqual(523537, indices[1]);
+    try std.testing.expectEqual(260131, indices[2]);
+    try std.testing.expectEqual(141919, indices[3]);
+    try std.testing.expectEqual(693377, indices[4]);
 }
